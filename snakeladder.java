5700049fa6e61a424e818bde398723435311aa6f
shekhar <shekharpatle999@gmail.com>
2021-04-07 01:24:02 +0530

    Added: code of checksforOption

--- snakeladder.java
+++ snakeladder.java
@@ -1,18 +1,44 @@
 
-public class ThePlayerRollsbetwn {
-  public static void main(String[] args) {
-
-	System.out.println("Wellcome to snake ladder game:");
+public class CheckForAOption {
+public static void main(String[] args) {
+	System.out.println("welcome to snake laddr game");
 	
-	  int startPosition=0;
-	  int minimum=1;
-	  int maximum=6;
+	int position=0;
+	int minimum=1;
+	int maximum=6;
+	
+	int randomValue=(int) Math.floor(Math.random()*(maximum-minimum+1)+ minimum);
+	int randomOption=(int) (Math.random()*10) % 3;
+	
+	if(randomOption == 0);
+	{
+		System.out.println("no play that's why player stay in same position which is" +position);
+		
+	}
 	  
-	  int randomValue=(int)Math.floor(Math.random()*(maximum-minimum+1));
-	  // Math.random()*(max-min+1)min
-	  
-	  System.out.println("Random number between 1 to 6 ="+randomValue);
-	  	  	  			  	
-}
-  
+	 if (randomOption == 1);
+	{
+		position += randomValue;
+		System.out.println("Ladder that's why player move ahead and new position is" +position);
+		
+	}
+	  if (randomOption == 2);
+	{
+	  if(position == 0)
+			
+		{
+			position =0;
+			
+		}
+		else
+		{
+			position -= randomValue;
+			
+		}
+		 System.out.println("snake that's why player move behind and new position is " + position);
+		
+	}
+		
+		
+	}
 }
