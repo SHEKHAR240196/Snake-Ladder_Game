be0687269e9d8e7e213a6a1e48839e5989cb4ed0
shekhar <shekharpatle999@gmail.com>
2021-04-07 01:17:41 +0530

    Added: code of theplayerrollsbetween

--- snakeladder.java
+++ snakeladder.java
@@ -1,10 +1,18 @@
 
-public class SnakeLadderGame {
-	
-	public static void main(String[] args) {
-		System.out.println("Welcome to snake ladder game:");
-		 
-		int strtPosition=0;
-	}
+public class ThePlayerRollsbetwn {
+  public static void main(String[] args) {
 
+	System.out.println("Wellcome to snake ladder game:");
+	
+	  int startPosition=0;
+	  int minimum=1;
+	  int maximum=6;
+	  
+	  int randomValue=(int)Math.floor(Math.random()*(maximum-minimum+1));
+	  // Math.random()*(max-min+1)min
+	  
+	  System.out.println("Random number between 1 to 6 ="+randomValue);
+	  	  	  			  	
+}
+  
 }
